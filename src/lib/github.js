459942560aec905c4d31b5d0e4fc8b2915e90f7c
Kresna Satya <kresnasatya72@gmail.com>
2024-12-10 22:24:48 +0800

    feat: test api/tags response

---
+++ src/lib/github.js
@@ -0,0 +1,103 @@
+import { GITHUB_TOKEN } from '$env/static/private';
+import { Octokit } from "octokit";
+
+/**
+ * @param {string} repository
+ */
+export async function allTags(repository) {
+	const options = process?.env.NODE_ENV === 'development' ? {
+		auth: GITHUB_TOKEN
+	} : {};
+
+	const octokit = new Octokit(options);
+
+	const [owner, repo] = repository.split('/', 2);
+
+	/**
+	 * @type {any[]}
+	 */
+	let tagNames = [];
+	for await (const response of octokit.paginate.iterator(
+		octokit.rest.repos.listTags,
+		{ owner, per_page: 100, repo }
+	)) {
+		tagNames = tagNames.concat(response.data.map((data) => data.name));
+	}
+
+	return tagNames;
+}
+
+/**
+ * @param {{ split: (arg0: string, arg1: number) => [any, any]; }} repository
+ * @param {any} sourceVersion
+ * @param {any} targetVersion
+ */
+export async function getDiff(repository, sourceVersion, targetVersion) {
+	try {
+		const octokit = new Octokit();
+
+		const [owner, repo] = repository.split('/', 2);
+		/**
+		 * @type {any[]}
+		 */
+		let files = [];
+		// Exclude filename when get patch between two versions.
+		const excludeFilenames = ['CHANGELOG.md', '.github', 'README.md'];
+
+		for await (const response of octokit.paginate.iterator(
+			octokit.rest.repos.compareCommitsWithBasehead,
+			{
+				basehead: `${sourceVersion}...${targetVersion}`,
+				owner,
+				per_page: 100,
+				repo
+			}
+		)) {
+			if (response.data.files) {
+				files = files.concat(response.data.files).filter((file) => {
+					return !excludeFilenames.some(prefix => file.filename.startsWith(prefix));
+				});
+			}
+		}
+
+		const result = files.map((/** @type {{ sha: any; filename: any; patch: string; }} */ item) => {
+			return {
+				sha: item.sha,
+				filename: item.filename,
+				source_url: `https://github.com/${repository}/blob/${sourceVersion}/${item.filename}`,
+				target_url: `https://github.com/${repository}/blob/${targetVersion}/${item.filename}`,
+				lines: parsedLines(item.patch),
+			}
+		});
+
+		return result;
+	} catch (error) {
+		throw new Error(`Cannot get diff between source version: ${sourceVersion} and target version: ${targetVersion}`);
+	}
+}
+
+/**
+ * @param {string} diffString
+ */
+function parsedLines(diffString) {
+	const lines = diffString.split('\n');
+
+    const parsedLines = lines.map((/** @type {string} */ line, /** @type {number} */ index) => {
+        let status = 'unchanged';
+
+        // Check the first character of the line to determine the status
+        if (line.startsWith('+')) {
+            status = 'add';
+        } else if (line.startsWith('-')) {
+            status = 'remove';
+        }
+
+        return {
+            number: index + 1,
+            text: line,
+            status: status
+        }
+    });
+
+    return parsedLines;
+}
--- src/routes/api/tags/+server.js
+++ src/routes/api/tags/+server.js
@@ -1,3 +1,37 @@
-export function GET() {
-    return new Response("Hello, Tags");
+import { allTags } from '$lib/github';
+import { FOOBAR } from '$env/static/private';
+
+export async function GET({ request, url }) {
+    const isCloudflare = typeof caches !== 'undefined';
+    
+    if (isCloudflare) {
+        const cacheKey = new Request(url.toString(), request);
+        const cache = caches.default;
+        let response = await cache.match(cacheKey);
+        if (response) return response;
+    }
+
+    let json = await allTags('laravel/laravel');
+
+    // let json = {
+    //     timestamp: new Date().toISOString(),
+    //     message: 'Hello there!',
+    //     secret: FOOBAR,
+    // };
+
+    let response = new Response(JSON.stringify(json));
+
+    response.headers.set('Cache-Control', 'public, max-age=3600');
+    response.headers.set('Content-Type', 'application/json');
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Allow-Methods', 'GET');
+
+    // Put the response in the cache
+    if (isCloudflare) {
+        const cache = caches.default;
+        const cacheKey = new Request(url.toString(), request);
+        cache.put(cacheKey, response.clone());
+    }
+
+    return response;
 }
