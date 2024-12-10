3ac94542df81515d098a04f41389cd65db32d450
Kresna Satya <kresnasatya72@gmail.com>
2024-12-11 07:49:27 +0800

    refactor: remove unnecessary functions

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,16 +1,1 @@
-async function allTags() {
-    const response = await fetch('https://workers.laraveldiff.org/api/tags');
-    return response.json();
-}
-
-export let tags = [...await allTags()];
-
-/**
- * @param {string} sourceVersion
- * @param {string} targetVersion
- */
-export async function getDiff(sourceVersion, targetVersion)
-{
-    const response = await fetch(`https://workers.laraveldiff.org/api/diff?source=${sourceVersion}&target=${targetVersion}`);
-    return response.json();
-}
+// place files you want to import through the `$lib` alias in this folder.
