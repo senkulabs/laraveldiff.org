285411aa133785f8210244accf166b4848af7d1e
Kresna Satya <kresnasatya72@gmail.com>
2024-12-14 17:11:53 +0800

    fix: correct display base and target versions in select element

--- src/lib/util.js
+++ src/lib/util.js
@@ -10,12 +10,52 @@ export function selectVersion(baseVersion, tags) {
         let base = baseVersion.replace('v', '').split('.').map(Number);
         let target = item.replace('v', '').split('.').map(Number);
 
-        // NOTE: We only allowed one version above the base version for incremental upgrade.
-        // This is intended for make upgrade process easier
-        // E.g. if the base version is 9 then it only allowed up to version 10.
-        
-        if (target[0] === (base[0] + 1) || target[0] === base[0]) {
-            return item;
+        // NOTE: For special cases upgrade.
+        // 4.0 to 4.2
+        // 4.2 to 5.0
+        // 5.0 to 5.1.x
+        // 5.1 to 5.2
+        // 5.2 to 5.3
+        // 5.3 to 5.4
+        // 5.4 to 5.5
+        // 5.5 to 5.6
+        // 5.6 to 5.7
+        // 5.7 to 5.8
+        // 5.8 to 6.0
+        switch (base[0]) {
+            case 4:
+                if (base[1] === 2) {
+                    return item.startsWith('v5.0');
+                } else {
+                    return item.startsWith('v4');
+                }
+            case 5:
+                switch (base[1]) {
+                    case 0:
+                        return item.startsWith('v5.1');
+                    case 1:
+                        return item.startsWith('v5.2');
+                    case 2:
+                        return item.startsWith('v5.3');
+                    case 3:
+                        return item.startsWith('v5.4');
+                    case 4:
+                        return item.startsWith('v5.5');
+                    case 5:
+                        return item.startsWith('v5.6');
+                    case 6:
+                        return item.startsWith('v5.7');
+                    case 7:
+                        return item.startsWith('v5.8');
+                }
+            default:
+                // NOTE: We only allowed one version above the base version for incremental upgrade.
+                // This is intended for make upgrade process easier
+                // E.g. if the base version is 9 then it only allowed up to version 10.
+                if (target[0] === (base[0] + 1) || target[0] === base[0]) {
+                    return item;
+                }
+                break;
         }
     });
     
