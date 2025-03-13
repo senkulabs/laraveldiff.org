afa5829b7d0459e168c061886791553bd8fd38e2
Kresna Satya <kresnasatya72@gmail.com>
2025-03-13 08:22:03 +0800

    fix: add response to not allow direct upgrade to latest version.
    E.g. version 5.5 to 12 (current latest) IS NOT allowed. But, version 5.5 to 5.6 is allowed. But, version 5.x to 6 IS NOT allowed. This is from Laravel docs upgrade section.

--- src/routes/api/diff/+server.js
+++ src/routes/api/diff/+server.js
@@ -10,6 +10,8 @@ export async function GET({ request, url }) {
         if (response) return response;
     }
 
+    const VERSION_5 = 5;
+    const VERSION_6 = 6;
     const baseVersion = url.searchParams.get('base');
     const targetVersion = url.searchParams.get('target');
 
@@ -62,6 +64,30 @@ export async function GET({ request, url }) {
                 });
             }
         }
+
+        if ((target[0] - base[0]) > 1) {
+            return new Response(`Direct upgrade from version ${baseVersion} to ${targetVersion} is not supported. Please upgrade incrementally!`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': 'GET, POST'
+                },
+                status: 400
+            });
+        }
+
+        // Special case when upgrade Laravel from version 5 to 6.
+        if (base[0] === VERSION_5 && target[0] == VERSION_6) {
+            return new Response(`Direct upgrade from version ${baseVersion} to ${targetVersion} is not supported. Please upgrade incrementally!`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': 'GET, POST'
+                },
+                status: 400
+            });
+        }
+        
 
         let json = await getDiff('laravel/laravel', baseVersion, targetVersion);
 
